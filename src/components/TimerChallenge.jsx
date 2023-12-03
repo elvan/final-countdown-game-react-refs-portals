6b8305f541f4cee24c64c98618a6bb09718b88bb
Elvan Hidayat <elvanhd@gmail.com>
2023-12-03 07:09:52 +0700

    using imperative handler

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -14,7 +14,7 @@ export default function TimerChallenge({ title, targetTime }) {
   function handleStart() {
     timer.current = setTimeout(() => {
       setTimerExpired(true);
-      dialog.current.showModal();
+      dialog.current.open();
     }, targetTime * 1000);
 
     setTimerStarted(true);
