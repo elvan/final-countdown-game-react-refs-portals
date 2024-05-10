7feed5ea3e3b1686313b58bef471f1b5aa5cbb83
Elvan Hidayat <elvanhd@gmail.com>
2024-05-10 08:53:09 +0700

    Add ResultModal and integrate with TimerChallenge
    - Introduce ResultModal component to display results
    - Import ResultModal in TimerChallenge
    - Display ResultModal when timer expires
    - Update TimerChallenge to use ResultModal for results
    - Adjust UI flow in TimerChallenge to accommodate modal

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,4 +1,5 @@
 import { useRef, useState } from 'react';
+import ResultModal from './ResultModal.jsx';
 
 // let timer;
 
@@ -21,20 +22,22 @@ export default function TimerChallenge({ title, targetTime }) {
   }
 
   return (
-    <section className='challenge'>
-      <h2>{title}</h2>
-      {timerExpired && <p>You lost!</p>}
-      <p className='challenge-time'>
-        {targetTime} second{targetTime > 1 ? 's' : ''}
-      </p>
-      <p>
-        <button onClick={timerStarted ? handleStop : handleStart}>
-          {timerStarted ? 'Stop' : 'Start'} Challenge
-        </button>
-      </p>
-      <p className={timerStarted ? 'active' : undefined}>
-        {timerStarted ? 'Time is running...' : 'Timer inactive'}
-      </p>
-    </section>
+    <>
+      {timerExpired && <ResultModal targetTime={targetTime} result='lost' />}
+      <section className='challenge'>
+        <h2>{title}</h2>
+        <p className='challenge-time'>
+          {targetTime} second{targetTime > 1 ? 's' : ''}
+        </p>
+        <p>
+          <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'} Challenge
+          </button>
+        </p>
+        <p className={timerStarted ? 'active' : undefined}>
+          {timerStarted ? 'Time is running...' : 'Timer inactive'}
+        </p>
+      </section>
+    </>
   );
 }
